079455c9fec5d3feab62522326c76dd7a3e820d2
김주영 <gimjuyeong0620@gmail.com>
2024-01-28 16:03:12 +0900

    dfs%bfs

--- src/main/java/codingtest/DFS.java
+++ src/main/java/codingtest/DFS.java
@@ -1,29 +1,69 @@
 package codingtest;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
-class Node{
-    int data;
-    Node lt, rt;
-    public Node(int val){
-        data = val;
-        lt = rt = null;
-    }
-}
+
 
 public class DFS {
+    static int[][] arr;
+    static boolean[] visited;
+
+    private void dfs(int V){
+        if(!visited[V]){
+            visited[V] = true;
+            System.out.print(V + " ");
+        }
+        for ( int i = 1 ; i < arr.length ; i++) {
+            if ( arr[V][i] == 1 && !visited[i]){
+                dfs(i);
+            }
+        }
+    }
+
+    private void bfs(int V){
+        if(!visited[V]){
+            visited[V] = true;
+            System.out.print(V + " ");
+        }
+        Queue<Integer> que = new LinkedList<Integer>();
+        que.add(V);
 
 
-    public int solution(int N){
-
-        return 0;
+        while(!que.isEmpty()){
+            Integer node = que.poll();
+            for ( int i = 1 ; i < arr.length ; i++) {
+                if (arr[node][i] == 1 && !visited[i]) {
+                    que.add(i);
+                    visited[i] = true;
+                    System.out.print(i + " ");
+                }
+            }
+        }
     }
 
     public static void main(String[] args){
         DFS T = new DFS();
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        int M = sc.nextInt();
+        int V = sc.nextInt();
 
-        System.out.println(T.solution(N));
+        arr = new int[N+1][N+1];
+        for (int i =0 ; i <M ; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr[a][b] = 1;
+            arr[b][a] = 1;
+        }
+
+        visited = new boolean[N+1];
+        T.dfs(V);
+
+        System.out.println();
+
+        visited = new boolean[N+1];
+        T.bfs(V);
     }
 }
