031c5d169975cc6d1e38705e721aec4cf5317535
김주영 <77871898+kjy-asl@users.noreply.github.com>
2024-06-15 15:22:05 +0900

    [Gold V] Title: 토마토, Time: 652 ms, Memory: 120648 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,100 +1,101 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int X, Y, Z;
-    static int[][][] arr,visited;
-    static Queue<Node324> q;
-    static int[] dy = {-1, 0, 1, 0, 0, 0};
-    static int[] dx = {0, -1, 0, 1, 0, 0};
-    static int[] dz = {0, 0, 0, 0, 1, -1};
 
-    public static void main(String[] args) {
-        Main m = new Main();
-        Scanner sc = new Scanner(System.in);
-        X = sc.nextInt();
-        Y = sc.nextInt();
-        Z = sc.nextInt();
-        sc.nextLine();
-        arr = new int[Z][Y][X];
-        visited = new int[Z][Y][X];
-        int minNum = 0;
-        q = new LinkedList<>();
-        for (int i = 0; i < Z; i++) {
-            for (int j = 0; j < Y; j++) {
-                for (int k = 0; k < X; k++) {
-                    arr[i][j][k] = sc.nextInt();
-                }
-            }
-        }
+    static int[][][] box;
+    static Queue<LocationOfTomato> locationOfTomatoes;
+    static int[] dx = {0,0,1,-1,0,0};
+    static int[] dy = {0,0,0,0,1,-1};
+    static int[] dz = {1,-1,0,0,0,0};
+    static int day = 0;
+    static int M,N,H;
 
-        for (int i = 0; i < Z ; i++) {
-            for (int j = 0; j < Y ; j++) {
-                for (int k = 0; k < X ; k++) {
-                    if (arr[i][j][k] == 1 && visited[i][j][k] == 0) {
-                        visited[i][j][k] = 1;
-                        q.add(new Node324(j,k,i));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        box = new int[H][N][M];
+
+        locationOfTomatoes = new LinkedList<LocationOfTomato>();
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < N; j++) {
+                st = new StringTokenizer(br.readLine());
+                for (int k = 0; k < M; k++) {
+                    int tomato = Integer.parseInt(st.nextToken());
+                    box[i][j][k] = tomato;
+                    if (tomato == 1) {
+                        locationOfTomatoes.offer(new LocationOfTomato(i, j, k));
                     }
                 }
             }
         }
-        bfs();
 
-        if (!checkTomato()) {
-            System.out.println(-1);
+        bfs(locationOfTomatoes);
+        if (isEveryTomatoesBoiled(box)) {
+            System.out.println(day);
         } else {
-            for (int i = 0; i < Z; i++) {
-                for (int j = 0; j < Y; j++) {
-                    for (int k = 0; k < X; k++) {
-                        if(minNum<arr[i][j][k]) minNum = arr[i][j][k];
+            System.out.println(-1);
+        }
+    }
+    public static void bfs(Queue<LocationOfTomato> locationOfTomatoes) {
+        if (locationOfTomatoes.isEmpty()) {
+            day = -1;
+        }
+
+        int sizeOfQueue = locationOfTomatoes.size();
+        while(sizeOfQueue!=0) {
+            LocationOfTomato locationOfTomato = locationOfTomatoes.poll();
+            int z = locationOfTomato.z;
+            int y = locationOfTomato.y;
+            int x = locationOfTomato.x;
+
+            for (int i = 0 ; i < 6; i++) {
+                int nextZ = z+dz[i];
+                int nextY = y+dy[i];
+                int nextX = x+dx[i];
+                if (nextZ>=0&&nextZ<H && nextY>=0&&nextY<N && nextX>=0&&nextX<M) {
+                    if(box[nextZ][nextY][nextX]==0) {
+                        box[nextZ][nextY][nextX] = 1;
+                        locationOfTomatoes.offer(new LocationOfTomato(nextZ, nextY, nextX));
                     }
                 }
             }
-
-            System.out.printf("%d", minNum-1);
-        }
-    }
-
-    public static void bfs() {
-        while(!q.isEmpty()) {
-            Node324 node = q.poll();
-            int z = node.z;
-            int y = node.y;
-            int x = node.x;
-            for (int i = 0 ; i < 6 ; i++) {
-                if (z+dz[i]>=0 && z+dz[i]<Z && x+dx[i]>=0 && x+dx[i]<X && y+dy[i]>=0 && y+dy[i]<Y) {
-                    if (arr[z+dz[i]][y+dy[i]][x+dx[i]] == 0) {
-                        arr[z+dz[i]][y+dy[i]][x+dx[i]] = arr[z][y][x]+1;
-                        visited[z+dz[i]][y+dy[i]][x+dx[i]] = 1;
-                        q.add(new Node324(y+dy[i],x+dx[i],z+dz[i]));
-                    }
+            sizeOfQueue-=1;
+            if (sizeOfQueue==0) {
+                sizeOfQueue = locationOfTomatoes.size();
+                if (sizeOfQueue!=0) {
+                    day+=1;
                 }
             }
         }
     }
 
-    public static boolean checkTomato() {
-        for (int i = 0; i < Z; i++) {
-            for (int j = 0; j < Y; j++) {
-                for (int k = 0; k < X; k++) {
-                    if (arr[i][j][k] == 0) {
-                        return false;
+    public static boolean isEveryTomatoesBoiled(int[][][] box) {
+        boolean EveryTomatoesBoiled = true;
+        for (int z = 0 ; z < H ; z++) {
+            for (int y = 0 ; y < N ; y++) {
+                for (int x = 0 ; x < M ; x++) {
+                    if (box[z][y][x] == 0) {
+                        EveryTomatoesBoiled = false;
+                        break;
                     }
                 }
             }
         }
-        return true;
+        return EveryTomatoesBoiled;
     }
 
-}
-
-class Node324 {
-    int y, x, z;
-
-    public Node324(int y, int x, int z) {
-        this.y = y;
-        this.x = x;
-        this.z = z;
+    static class LocationOfTomato {
+        int z,y,x;
+        private LocationOfTomato(int z, int y, int x) {
+            this.z = z;
+            this.y = y;
+            this.x = x;
+        }
     }
 }
